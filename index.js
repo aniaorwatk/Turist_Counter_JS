c62314b69bb6b9da838894168780051b1bf69989
ania orwatk <orwat.ania@gmail.com>
2023-01-23 09:57:47 +0100

    create app

---
+++ index.js
@@ -0,0 +1,24 @@
+let saveEl = document.querySelector(".saveEl")
+let countEl = document.querySelector(".countEl")
+let count = 0
+let number = ""
+
+const increment=()=> {
+    count += 1
+    countEl.textContent = count
+}
+console.log(countEl)
+const save = () => {
+
+    if (!number.length) {
+        countStr = `${count} `;
+    }
+
+    else if (number.length) {
+        countStr = `- ${count} `;
+    }
+    number += countStr
+    saveEl.textContent = `Previous entries: ${number}`;
+    count = 0
+    countEl.textContent = 0
+}
