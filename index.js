ffe022b6a44076af63e6fbd32dc8ba7ad752c431
ania orwatk <orwat.ania@gmail.com>
2023-03-05 14:30:42 +0100

    adding the condition, maximum people in the group

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 let saveEl = document.querySelector(".saveEl");
 let countEl = document.querySelector(".countEl");
 let sumEl = document.querySelector(".sumEl");
+let messageEl = document.querySelector(".messageEl");
 let count = 0;
 let number = "";
-let sum = 0
 
 const increment = () => {
     count += 1;
+    if (count === 10) {
+        count -= 1
+        messageEl.textContent = "One group can have a maximum of 9 people!"
+    }
     countEl.textContent = count;
 }
 
 const save = () => {
-    if(count === 0){
+    if (count === 0) {
         countStr = ""
-    } else if(!number.length) {
+    } else if (!number.length) {
         countStr = `${count} `;
     } else if (number.length) {
         countStr = `- ${count} `;
@@ -22,15 +26,16 @@ const save = () => {
     saveEl.textContent = `Previous entries: ${number}`;
     count = 0;
     countEl.textContent = 0;
+    messageEl.textContent = ""
 }
 
 const sumTurists = () => {
     let array = [...number]
         .filter(number => {
             return number !== "-" && number !== " "
-        }).map(element => parseInt(element))
+        })
+        .map(element => parseInt(element))
         .reduce((a, b) => a + b, 0)
-
     sumEl.textContent = `Sum: ${array}`
 }
 
@@ -39,5 +44,6 @@ const resets = () => {
     number = "";
     sumEl.textContent = `Sum:`;
     countEl.textContent = 0;
-    saveEl.textContent = `Previous entries:` ;
+    saveEl.textContent = `Previous entries:`;
+    messageEl.textContent = ""
 }
